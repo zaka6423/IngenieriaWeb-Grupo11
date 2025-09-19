7b396c5c8ddaa691aead04781c2de5d2c109568e
Gonzalo Caluva <gonzalocaluva17@gmail.com>
2025-09-19 17:59:05 -0300

    fix superuser

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ uv sync
 cd $(dirname $(find . | grep manage.py$))
 uv run ./manage.py collectstatic --no-input
 uv run ./manage.py migrate
+export DJANGO_SUPERUSER_PASSWORD=admin123
 uv run ./manage.py createsuperuser --username admin --email "zacariasapoca@gmail.com" --noinput || true
